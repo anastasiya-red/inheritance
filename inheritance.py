3d2c4c769cd1b59c285d64d22278d5e51d9db1c5
Anastasiya <165375443+anastasiya-red@users.noreply.github.com>
2024-04-20 21:14:21 +0300

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,34 @@
+class Car:
+    price = 1000000
+
+    def __init__(self, horsepower = 100):
+        self.horsepower = horsepower
+
+    def horse_powers(self, horsepower = 100):
+        return (f'У Car лошадиных сил {self.horsepower}')
+
+
+class Nissan(Car):
+    price = 7000000
+
+    def horse_powers(self):
+        return print(f'У Nissan лошадиных сил {self.horsepower}')
+
+
+class Kia(Car):
+    price = 5000000
+
+    def horse_powers(self):
+        return print(f'У Kia лошадиных сил {self.horsepower}')
+
+niva = Car(100)
+navara = Nissan(190)
+stinger = Kia(370)
+navara.horse_powers()
+stinger.horse_powers()
+
+print(niva.horse_powers())
+
+print(niva.price)
+print(navara.price)
+print(stinger.price)
